00e2038b1fe0adc76ca024c8525dae5813e422e0
wang xing <897304222@qq.com>
2018-06-22 09:34:30 +0800

    wangxing

---
+++ jsppse/code/1-js基础回顾/01-js基础回顾.js
@@ -0,0 +1,67 @@
+/*
+ 
+ *  1、js的定义？
+ *   简单的理解做网页的交互效果。
+ * 
+ * 2、js的组成部分？
+ *   1)ECMAScript（核心）
+ *     ES3 ES5  ES6(ES2015)  ES7(2016)  ES8(2017)
+ * 
+ *      变量
+ * 		数据类型：
+ *   	  基本的数据类型：String Number Boolean Null Undefined
+ * 		 引用数据类型：Object(Array Function)
+ * 		运算符
+ *        + - * / ++  -- = == === % ||  && ! > < 
+ * 			三元运算符：  a?b:c
+ * 
+ * 		条件判断
+ * 			if else
+ * 			switch
+ * 		循环
+ * 			for while
+ * 		函数：
+ *        函数的定义： 
+ * 			function 函数名(参数){
+			 * 	//函数体
+			}
+			
+		  使用函数:
+		  	函数名(参数)
+		  	
+		正则表达式:/要匹配的规则/
+		  验证手机号码：/^1[3456789][0123456789]{9}/
+ * 
+ * 
+ *   2)DOM
+ *   	获取元素：
+ * 			document.getElementById();
+ * 			document.getElementsByTagName();
+ * 			document.getElementsByName();
+ * 			document.getElementsByClassName();
+ * 			querySelector()
+ * 
+ * 		操作：
+ * 			ele.getAttribute
+ * 			ele.setAttribute
+ * 			ele.style.color
+ * 			ele.innerHTML(如果存在标签，会渲染)
+ * 			ele.innerText(全部作文本输出)
+ * 
+ * 		动态生成标签：
+ * 			createElement
+ * 			createTextNode
+ * 			appendChild
+ * 
+ *   3)BOM
+ * 		location
+ * 		history
+ * 		navigator
+ * 		screen
+ * 		计时器
+ * 		alert
+ * 		console
+ * 		XMLHttpRequest（AJAX）
+ * 
+ * 
+ * */
---
+++ jsppse/code2/js/fn.js
@@ -0,0 +1,4 @@
+//定义输出到控制台函数
+function l(msg){
+	console.log(msg);
+}
